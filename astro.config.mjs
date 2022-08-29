83c91c14aec03588c064d11a524af03a2477f4e8
Jean-Baptiste Mariage <jbm@inniti.de>
2022-08-29 08:04:45 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import node from '@astrojs/node'
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [preact()],
+	adapter: node(),
+});
---
+++ src/components/compteur.jsx
@@ -0,0 +1,8 @@
+
+function Nav() {
+	return (
+        <h1>Hello world</h1>
+		
+	);
+}
+export default Nav;
